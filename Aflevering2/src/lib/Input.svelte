3b393deccdcff1d38f19079508a7d0d8dadfe3a2
ahmad mukhtar alizadeh <ahm6721@gmail.com>
2025-05-03 14:36:35 +0200

    Første commit

---
+++ Aflevering2/src/lib/Input.svelte
@@ -0,0 +1,6 @@
+<script>
+    let { value = $bindable('') } = $props();
+  </script>
+  
+  <input class="input m-2" bind:value placeholder="Brugernavn" />
+  
---
+++ Aflevering2/src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    import Input from '../lib/Input.svelte';
+    import Button from '../lib/Button.svelte';
+  
+    let userName = '';
+    let password = '';
+  
+    function btnClick() {
+      if (userName === 'Nancy' && password === 'Kode') {
+        alert('Login successful');
+      } else {
+        alert('Login failed');
+      }
+    }
+  </script>
+  
+  <div class="flex flex-col items-center justify-center min-h-screen bg-base-200">
+    <div class="bg-white p-8 rounded shadow-md w-80">
+      <h1 class="text-3xl font-bold mb-6 text-center">Login</h1>
+  
+      <Input bind:value={userName} placeholder="Brugernavn" />
+      <Input bind:value={password} type="password" placeholder="Kodeord" />
+  
+      <Button onclick={btnClick}>
+        Login
+      </Button>
+    </div>
+  </div>
+  
