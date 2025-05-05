d86e7bb4a677e67b2d221bc4ed886e07b59da9b8
ahmad mukhtar alizadeh <ahm6721@gmail.com>
2025-05-05 16:46:44 +0200

    clean up

--- Aflevering2/src/lib/Button.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  let { children, onclick } = $props();
-</script>
-
-<button class="btn btn-primary m-2" {onclick}>
-  {@render children()}
-</button>
--- Aflevering2/src/lib/Input.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-    let { value = $bindable('') } = $props();
-  </script>
-  
-  <input class="input m-2" bind:value placeholder="Brugernavn" />
-  
